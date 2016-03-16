b11549c40b822214dc7c78a510e5244c964fca7c
pelitoren <pelitoren 1 at gmail dot com>
2016-03-15 19:30:16 -0600

    made bones and gunpowder drop for gravel

---
+++ betaexpansion/BlockGravel.java
@@ -0,0 +1,36 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) braces deadcode 
+
+package net.minecraft.src;
+
+import java.util.Random;
+
+// Referenced classes of package net.minecraft.src:
+//            BlockSand, Item
+
+public class BlockGravel extends BlockSand
+{
+
+    public BlockGravel(int i, int j)
+    {
+        super(i, j);
+    }
+
+    public int idDropped(int i, Random random)
+    {
+        if(random.nextInt(10) == 0)
+        {
+            if (random.nextInt(1) == 0){
+                if (random.nextInt(2) == 0){
+                    return Item.gunpowder.shiftedIndex;
+                }
+                return Item.bone.shiftedIndex;
+            }
+            return Item.flint.shiftedIndex;
+        } else
+        {
+            return blockID;
+        }
+    }
+}
