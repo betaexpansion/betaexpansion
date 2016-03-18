6c387b399cdedf7e679fff2b05fd18d9967f3559
pelitoren <pelitoren 1 at gmail dot com>
2016-03-18 15:23:24 -0600

    added net physics

--- betaexpansion/BlockNet.java
+++ betaexpansion/BlockNet.java
@@ -9,11 +9,50 @@ public class BlockNet extends ModBlock
 		super(i, j, Material.cloth, name);
         setBlockBounds(0.0f, 14.0f / 16.0f, 0.0f, 
                        1.0f, 15.0f / 16.0f, 1.0f);
+        setTickOnLoad(true);
 	}
 
     public boolean isOpaqueCube()
     {
         return false;
+    }
+    
+    @Override
+    public boolean canPlaceBlockAt(World world, int i, int j, int k)
+    {
+        return validPos(world, i, j, k); 
+    }
+    
+    @Override
+    public void onNeighborBlockChange(World world,
+                                      int i, int j, int k, int l)
+    {
+        if (!validPos(world, i, j, k)){
+            dropBlockAsItem(world, i, j, k, 
+                            world.getBlockMetadata(i, j, k));
+            world.setBlockWithNotify(i, j, k, 0);
+        }
+    }
+    
+    @Override
+    public void updateTick(World world, int i, int j, int k, Random random)
+    {
+        if (!validPos(world, i, j, k)){
+            dropBlockAsItem(world, i, j, k, 
+                            world.getBlockMetadata(i, j, k));
+            world.setBlockWithNotify(i, j, k, 0);
+        }
+    }
+
+    public boolean validPos(World world, int i, int j, int k)
+    {
+        int count = 0;
+        if (world.getBlockId(i + 1, j, k) != 0){ count++; }
+        if (world.getBlockId(i - 1, j, k) != 0){ count++; }
+        if (world.getBlockId(i, j, k + 1) != 0){ count++; }
+        if (world.getBlockId(i, j, k - 1) != 0){ count++; }
+        if (world.getBlockId(i, j - 1, k) != 0){ count += 10; }
+        return count >= 2;
     }
 
 }
